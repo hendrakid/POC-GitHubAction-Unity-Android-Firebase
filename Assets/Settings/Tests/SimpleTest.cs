0623991c252a73dcffbd7fab72ff68ab0cadc5ff
kholilurrohman.hendrata <kholilurrohman.hendrata@ecomindo.com>
2024-11-18 17:07:55 +0700

    Initial commit

---
+++ Assets/Settings/Tests/SimpleTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+
+public class SimpleTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestDownload()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator TestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
