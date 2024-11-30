c96862f477d697da472213bbea8ff72afa69b028
kholilurrohman.hendrata <kholilurrohman.hendrata@ecomindo.com>
2024-11-30 19:28:46 +0700

    uncomment gps

--- Assets/Scripts/PlayGamesManager.cs
+++ Assets/Scripts/PlayGamesManager.cs
@@ -1,43 +1,43 @@
-//using GooglePlayGames;
-//using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 using UnityEngine;
 
 public class PlayGamesManager : MonoBehaviour
 {
-    //void Start()
-    //{
-    //    SignIn();
-    //}
-    //public void SignInManually()
-    //{
-    //    PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
-    //}
+    void Start()
+    {
+        SignIn();
+    }
+    public void SignInManually()
+    {
+        PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
+    }
 
-    //public void SignIn()
-    //{
-    //    PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
-    //}
+    public void SignIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
 
-    //internal void ProcessAuthentication(SignInStatus status)
-    //{
-    //    if (status == SignInStatus.Success)
-    //    {
-    //        // Continue with Play Games Services
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
 
-    //        string name = PlayGamesPlatform.Instance.GetUserDisplayName();
-    //        string id = PlayGamesPlatform.Instance.GetUserId();
-    //        string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+            string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+            string id = PlayGamesPlatform.Instance.GetUserId();
+            string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
 
-    //        Debug.Log("Success \n " + name);
+            Debug.Log("Success \n " + name);
 
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Sign in Failed!!");
+        }
+        else
+        {
+            Debug.Log("Sign in Failed!!");
 
-    //        // Disable your integration with Play Games Services or show a login button
-    //        // to ask users to sign-in. Clicking it should call
-    //        // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
-    //    }
-    //}
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        }
+    }
 }
